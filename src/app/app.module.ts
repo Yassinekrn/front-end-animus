c9e269ae81f0e384a203bac4989db9e73690a420
noel_vi <krichenyassine22@gmail.com>
2023-11-04 23:22:12 +0100

    init of project

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { FooterComponent } from './components/homepage/footer/footer.component';
+import { NavbarComponent } from './components/homepage/navbar/navbar.component';
+import { AboutusComponent } from './components/home/aboutus/aboutus.component';
+import { DiscussionListComponent } from './components/events/discussion-list/discussion-list.component';
+import { DiscussionComponent } from './components/events/discussion/discussion.component';
+import { DiscussionFormComponent } from './components/forms/discussion-form/discussion-form.component';
+import { AnimeFormComponent } from './components/forms/anime-form/anime-form.component';
+import { AnimeListComponent } from './components/anime_ranks/anime-list/anime-list.component';
+import { AnimeComponent } from './components/anime_ranks/anime/anime.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    FooterComponent,
+    NavbarComponent,
+    AboutusComponent,
+    DiscussionListComponent,
+    DiscussionComponent,
+    DiscussionFormComponent,
+    AnimeFormComponent,
+    AnimeListComponent,
+    AnimeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/forms/anime-form/anime-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anime-form',
+  templateUrl: './anime-form.component.html',
+  styleUrls: ['./anime-form.component.css']
+})
+export class AnimeFormComponent {
+
+}
